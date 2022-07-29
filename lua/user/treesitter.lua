f9a61c4d8b894987769e829f5c963a6d910281c9
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-07-29 11:30:05 +0200

    style: update formatting

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -22,13 +22,25 @@ parser_config.puppet = {
 
 configs.setup({
   ensure_installed = "all", -- one of "all" or a list of languages
+  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
+  matchup = {
+    enable = true, -- mandatory, false will disable the whole extension
+  },
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = { "css" }, -- list of language that will be disabled
+    disable = { "css", "markdown" }, -- list of language that will be disabled
   },
   autopairs = {
     enable = true,
   },
   indent = { enable = true, disable = { "python", "css" } },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
+  autotag = {
+    enable = true,
+    disable = { "xml", "markdown" },
+  },
 })
