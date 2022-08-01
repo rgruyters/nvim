8f67b1366d3bca2e6c0997655038c7389666396d
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-08-01 13:42:11 +0200

    feat: added tabline toggle

--- lua/user/functions.lua
+++ lua/user/functions.lua
@@ -22,6 +22,20 @@ function M.toggle_diagnostics()
   end
 end
 
+function M.toggle_tabline()
+  local value = vim.api.nvim_get_option_value("showtabline", {})
+
+  if value == 2 then
+    value = 0
+  else
+    value = 2
+  end
+
+  vim.opt.showtabline = value
+
+  vim.notify("showtabline" .. " set to " .. tostring(value))
+end
+
 function M.isempty(s)
   return s == nil or s == ""
 end
