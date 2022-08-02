69ce2153cc09dfbfb9ad4a5ba979e816007f76fc
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-08-02 13:07:55 +0200

    fix: do not install all languages

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -21,7 +21,19 @@ parser_config.puppet = {
 }
 
 configs.setup({
-  ensure_installed = "all", -- one of "all" or a list of languages
+  ensure_installed = {
+    "hcl",
+    "bash",
+    "puppet",
+    "python",
+    "markdown",
+    "markdown_inline",
+    "dockerfile",
+    "json",
+    "lua",
+    "vim",
+    "yaml",
+  }, -- one of "all" or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   matchup = {
