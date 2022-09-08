01fd0358223e8c97a4cfcc49e7b6688d334e6260
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-09-08 21:04:54 +0200

    fix: disable completion for Markdown

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -10,7 +10,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "TelescopePrompt" },
+  pattern = { "TelescopePrompt", "markdown" },
   callback = function()
     require("cmp").setup.buffer({ enabled = false })
   end,
