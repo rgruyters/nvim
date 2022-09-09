85e7ce42430b724d7bb719fb0e4978f4f2444aeb
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-09-09 12:42:35 +0200

    feat: added Undotree plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -147,6 +147,9 @@ return packer.startup(function(use)
   -- Alignment
   use { "Vonr/align.nvim" } -- Aligning lines
 
+  -- UndoTree
+  use{ "mbbill/undotree" }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
