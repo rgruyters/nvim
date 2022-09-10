f7d57bf6e2e06fff2a770a782e03d93d4bb8ed79
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-09-10 12:42:24 +0200

    feat: use different color theme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -72,6 +72,7 @@ return packer.startup(function(use)
 
   -- Colorschemes
   use { "shaunsingh/nord.nvim" } -- Nord theme: My theme of choise
+  use { "catppuccin/nvim", as = "catppuccin" } -- catppuccin theme
 
   -- cmp plugins
   use { "hrsh7th/nvim-cmp" } -- The completion plugin
