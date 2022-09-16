68f9466a39545a3ae3b696cdeba865923d6e9e4b
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-09-16 10:57:24 +0200

    feat: added shiftround

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -22,6 +22,7 @@ vim.opt.updatetime = 300                        -- faster completion (4000ms def
 vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 vim.opt.expandtab = true                        -- convert tabs to spaces
 vim.opt.shiftwidth = 2                          -- the number of spaces inserted for each indentation
+vim.opt.shiftround = true                       -- honour indenting
 vim.opt.tabstop = 2                             -- insert 2 spaces for a tab
 vim.opt.cursorline = true                       -- highlight the current line
 vim.opt.number = true                           -- set numbered lines
