f26098db1e9e4f6e81d9c7b911949d57a28330c9
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-09-20 13:08:02 +0200

    feat: added eol character

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -41,3 +41,5 @@ vim.opt.fillchars.eob=" "
 vim.opt.shortmess:append "c"
 vim.opt.whichwrap:append("<,>,[,],h,l")
 vim.opt.iskeyword:append("-")
+vim.opt.list = false
+vim.opt.listchars:append "eol:$"
