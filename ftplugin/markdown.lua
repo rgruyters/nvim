d7fac3f75c1933be54f71125e400ade28e0f916b
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-09-21 12:37:36 +0200

    fix: remove unused whichkey

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,22 +1,3 @@
-local status_ok, which_key = pcall(require, "which-key")
-if not status_ok then
-  vim.notify("Cannot load which-key for Markdown")
-  return
-end
-
 vim.cmd("set tw=80 wrap linebreak")
 
-local opts = {
-  mode = "n", -- NORMAL mode
-  prefix = "<leader>",
-  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
-  noremap = true, -- use `noremap` when creating keymaps
-  nowait = true, -- use `nowait` when creating keymaps
-}
-
-local mappings = {
-  m = { "<cmd>MarkdownPreview<cr>", "Markdown Preview" },
-}
-
-which_key.register(mappings, opts)
+vim.keymap.set("n", "<leader>m", "<cmd>MarkdownPreview<CR>", { silent = true })
