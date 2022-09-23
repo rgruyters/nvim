648884b177d7e62059f972dfb90c557ef2918b1e
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-09-23 11:16:16 +0200

    refactor: moved puppet treesitter config to separate folder

---
+++ after/ftplugin/puppet.lua
@@ -0,0 +1,14 @@
+local config_ok, parsers = pcall(require, "nvim-treesitter.parsers")
+if not config_ok then
+  return
+end
+
+local parser_config = parsers.get_parser_configs()
+parser_config.puppet = {
+  install_info = {
+    url = "https://github.com/neovim-puppet/tree-sitter-puppet.git",
+    branch = "main",
+    files = { "src/parser.c", "src/scanner.cc" },
+  },
+  filetype = "puppet",
+}
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -4,22 +4,6 @@ if not status_configs_ok then
   return
 end
 
-local status_parsers_ok, parsers = pcall(require, "nvim-treesitter.parsers")
-if not status_parsers_ok then
-  vim.notify("Error loading treesitter parsers")
-  return
-end
-
-local parser_config = parsers.get_parser_configs()
-parser_config.puppet = {
-  install_info = {
-    url = "https://github.com/neovim-puppet/tree-sitter-puppet.git",
-    branch = "main",
-    files = { "src/parser.c", "src/scanner.cc" },
-  },
-  filetype = "puppet",
-}
-
 configs.setup({
   ensure_installed = {
     "hcl",
