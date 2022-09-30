57fcfbd3532ae86fc3b12c6ebb94df412c05ce0d
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-09-30 11:27:16 +0200

    style: Use Catppuccin theme... for now

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,4 +1,12 @@
-local colorscheme = "nord"
+local catppuccin_status_ok, catppuccin = pcall(require, "catppuccin")
+if not catppuccin_status_ok then
+  return
+end
+
+local colorscheme = "catppuccin"
+
+vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+catppuccin.setup()
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
