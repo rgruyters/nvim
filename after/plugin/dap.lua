ca5d2b3119028a1eafe478e74cf0db0181b0aa22
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-11-21 10:33:22 +0100

    chore: update DAP settings

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -8,7 +8,51 @@ if not dap_ui_status_ok then
     return
 end
 
-dapui.setup()
+local dap_install_status_ok, dap_install = pcall(require, "dap-install")
+if not dap_install_status_ok then
+	return
+end
+
+dap_install.setup({})
+
+dap_install.config("python", {})
+-- add other configs
+
+dapui.setup({
+    expand_lines = true,
+	icons = { expanded = "", collapsed = "", circular = "" },
+	mappings = {
+		-- Use a table to apply multiple mappings
+		expand = { "<CR>" },
+		open = "o",
+		remove = "d",
+		edit = "e",
+		repl = "r",
+		toggle = "t",
+	},
+	layouts = {
+		{
+			elements = {
+				{ id = "scopes", size = 0.33 },
+				{ id = "breakpoints", size = 0.17 },
+				{ id = "stacks", size = 0.25 },
+				{ id = "watches", size = 0.25 },
+			},
+			size = 0.33,
+			position = "right",
+		},
+		{
+			elements = {
+				{ id = "repl", size = 0.45 },
+				{ id = "console", size = 0.55 },
+			},
+			size = 0.27,
+			position = "bottom",
+		},
+	},
+})
+
+vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
     dapui.open {}
