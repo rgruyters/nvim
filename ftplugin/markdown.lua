f47bddc76a3c28c43a92934df5c1b6aef53f038e
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-12-18 21:10:39 +0100

    chore: update keymaps for keymaps and markdown

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,3 +1,3 @@
 vim.cmd("set tw=80 wrap linebreak")
 
-vim.keymap.set("n", "<leader>m", "<cmd>MarkdownPreview<CR>", { silent = true })
+vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreview<CR>", { silent = true, desc = "[M]arkdown [P]review" })
