c15f2ca0a5a2f3a0ced2a0c299bc1a498d1b13ce
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-12-19 16:58:15 +0100

    chore: remove default settings for Bufferline

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,11 +1,10 @@
 local bufferline_loaded, bufferline = pcall(require, "bufferline")
 if not bufferline_loaded then
-  return
+    return
 end
 
 bufferline.setup {
   options = {
-    close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
     separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
     show_buffer_close_icons = false,
     show_close_icon = false,
