eaffd60c57f7f972fce25a3848ef88b52169d8c8
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-01-05 16:52:04 +0100

    chore: add priority for colorscheme

--- lua/grtrs/plugins.lua
+++ lua/grtrs/plugins.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     {
         "catppuccin/nvim",
         name = "catppuccin",
+        priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
             vim.cmd.colorscheme "catppuccin-macchiato"
         end
