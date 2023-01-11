abf052d415490b3de36071eaa31198d867d4acfd
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-01-11 14:31:47 +0100

    feat: add snippets

--- lua/grtrs/lsp/init.lua
+++ lua/grtrs/lsp/init.lua
@@ -32,13 +32,52 @@ lsp.set_preferences({
   set_lsp_keymaps = false
 })
 
+local cmp_loaded, cmp = pcall(require, "cmp")
+if not cmp_loaded then
+    return
+end
+local luasnip_loaded, luasnip = pcall(require, "luasnip")
+if not luasnip_loaded then
+    return
+end
+
 -- Update completion via lsp-zero
 lsp.setup_nvim_cmp({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    }),
     sources = {
         { name = 'path' },
-        { name = 'nvim_lsp', keyword_length = 3 },
-        { name = 'buffer', keyword_length = 3 },
-        { name = 'luasnip', keyword_length = 2 },
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+        { name = 'luasnip' },
     }
 })
 
