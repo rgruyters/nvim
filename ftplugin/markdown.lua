b88c5e4613ca52a0547d54d5caa24547be3bf76e
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-01-16 13:09:37 +0100

    chore: add keymap for settings current datetime

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,3 +1,4 @@
 vim.cmd("set tw=80 wrap linebreak")
 
 vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreview<CR>", { silent = true, desc = "[M]arkdown [P]review" })
+vim.keymap.set("i", "dts", "<C-R>=strftime(\"%FT%T%z\")<CR>", { silent = true, desc = "Insert current [d]ate[t]ime [s]tring"})
