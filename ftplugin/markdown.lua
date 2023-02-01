79f4af91944acf4e709abe8dd2bd2c9e2fab4b35
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-02-01 17:04:09 +0100

    chore: update strftime to shorten timezone output

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,4 +1,4 @@
 vim.cmd("set tw=80 wrap linebreak")
 
 vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreview<CR>", { silent = true, desc = "[M]arkdown [P]review" })
-vim.keymap.set("i", "dts", "<C-R>=strftime(\"%FT%T%z\")<CR>", { silent = true, desc = "Insert current [d]ate[t]ime [s]tring"})
+vim.keymap.set("i", "dts", "<C-R>=strftime(\"%FT%T+1\")<CR>", { silent = true, desc = "Insert current [d]ate[t]ime [s]tring"})
