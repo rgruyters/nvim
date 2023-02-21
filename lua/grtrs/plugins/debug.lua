495075a7fbc668283abf55c891e1ab6c6cff220c
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-02-21 20:49:02 +0100

    fix: remove automatic setup

--- lua/grtrs/plugins/debug.lua
+++ lua/grtrs/plugins/debug.lua
@@ -17,18 +17,7 @@ return {
 
         require("telescope").load_extension("dap")
 
-        require("mason-nvim-dap").setup {
-            -- Makes a best effort to setup the various debuggers with
-            -- reasonable debug configurations
-            automatic_setup = true,
-
-            -- You'll need to check that you have the required things installed
-            -- online, please don't ask me how to install them :)
-            ensure_installed = {
-                -- Update this to ensure that you have the debuggers for the langs you want
-                "debugpy",
-            },
-        }
+        require("mason-nvim-dap").setup {}
 
         -- You can provide additional configuration to the handlers,
         -- see mason-nvim-dap README for more information
