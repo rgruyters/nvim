b7e1f4dca714a75ca046a8d3cde18cf114689d56
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-03-14 10:10:53 +0100

    chore: update keymaps for lsp
    - add mode option for setting keymap for particuler mode
    - allow showing lsp signature while in insert mode

--- lua/grtrs/lsp/handlers.lua
+++ lua/grtrs/lsp/handlers.lua
@@ -42,38 +42,36 @@ M.setup = function()
 end
 
 local function lsp_keymaps(bufnr)
-    local nmap = function(keys, func, desc)
+    local kmap = function(mode, keys, func, desc)
         if desc then
             desc = 'LSP: ' .. desc
         end
 
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+        vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = desc })
     end
 
-    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    kmap('n', '<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    kmap('n', '<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-    nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-    nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-    nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+    kmap('n', 'gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+    kmap('n', 'gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    kmap('n', 'gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+    kmap('n', '<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+    kmap('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+    kmap('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
     -- See `:help K` for why this keymap
-    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+    kmap('n', 'K', vim.lsp.buf.hover, 'Hover Documentation')
+    kmap({'n', 'i'}, '<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
     -- Lesser used LSP functionality
-    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-    nmap('<leader>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, '[W]orkspace [L]ist Folders')
+    kmap('n', 'gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    kmap('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+    kmap('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+    kmap('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, '[W]orkspace [L]ist Folders')
 
     -- LSP Formatting
-    nmap('<leader>f', vim.lsp.buf.format, '[F]ormat current buffer with LSP')
+    kmap('n', '<leader>f', vim.lsp.buf.format, '[F]ormat current buffer with LSP')
 end
 
 local function lsp_highlight_document(client)
