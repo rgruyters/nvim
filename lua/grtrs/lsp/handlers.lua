11111d38d0412393cf90f345a3c37bb5ba1add67
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-03-16 14:50:57 +0100

    fix: missing borders for diag and signature

--- lua/grtrs/lsp/handlers.lua
+++ lua/grtrs/lsp/handlers.lua
@@ -32,6 +32,14 @@ M.setup = function()
         update_in_insert = true,
         underline = true,
         severity_sort = true,
+		float = {
+			focusable = true,
+			style = "minimal",
+			border = "rounded",
+			source = "always",
+			header = "",
+			prefix = "",
+		},
         automatic_configuration = {
             ---@usage list of filetypes that the automatic installer will skip
             skipped_filetypes = skipped_filetypes,
@@ -39,6 +47,14 @@ M.setup = function()
     }
 
     vim.diagnostic.config(config)
+
+	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+		border = "rounded",
+	})
+
+	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+		border = "rounded",
+	})
 end
 
 local function lsp_keymaps(bufnr)
