9e0dd7dfcdbd8868e7039469d48240e8112d5498
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-03-18 13:00:36 +0100

    fix: rebase option didn't work

--- lua/grtrs/plugins/core.lua
+++ lua/grtrs/plugins/core.lua
@@ -91,7 +91,7 @@ return {
         init = function()
             vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus"} )
             vim.keymap.set("n", "<leader>gp", function() vim.cmd.Git('push') end, { desc = "[G]it [p]ush"} )
-            vim.keymap.set("n", "<leader>gP", function() vim.cmd.Git({'pull',  '--rebase'}) end, { desc = "Git [P]ull"} )
+            vim.keymap.set("n", "<leader>gP", function() vim.cmd.Git('pull') end, { desc = "Git [P]ull"} )
         end,
         event = "VeryLazy" },
     -- Superfast Git decorations
