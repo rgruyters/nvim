1bc40b8cb0c4728ea1faffb87a45cd149d2f4ddd
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-03-18 12:57:44 +0100

    docs: add default options and comments for lazy

--- lua/grtrs/lazy.lua
+++ lua/grtrs/lazy.lua
@@ -17,7 +17,15 @@ require("lazy").setup({
         { import = "grtrs.plugins" },
     },
     install = { colorscheme = { "catppuccin" } },
-    checker = { enabled = false },
+    checker = { enabled = false }, -- do not check automatically for plugin updates
+    defaults = {
+        -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+        lazy = false,
+        -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+        -- have outdated releases, which may break your Neovim install.
+        version = false, -- always use the latest git commit
+        -- version = "*", -- try installing the latest stable version for plugins that support semver
+    },
     performance = {
         rtp = {
             -- disable some rtp plugins that I don't use in daily life
