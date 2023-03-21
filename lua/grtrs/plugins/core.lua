7b6bc796ebd62499d18ec91e3af9c7e968a98d85
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-03-21 09:45:43 +0100

    fix: disable lazy loading for cmp

--- lua/grtrs/plugins/core.lua
+++ lua/grtrs/plugins/core.lua
@@ -60,7 +60,6 @@ return {
 
     -- Autocompletion
     { "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
         dependencies = {
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
