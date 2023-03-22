f9d76c25c9852634d8002b973d8c41872a5c5c9d
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-03-22 10:57:14 +0100

    feat: add lir plugin

--- lua/grtrs/plugins/custom.lua
+++ lua/grtrs/plugins/custom.lua
@@ -60,4 +60,13 @@ return {
         end,
         ft = { "markdown" },
     },
+
+    -- Alternative File Explorer
+    {
+        "tamago324/lir.nvim",
+        keys = {
+            { "<leader>e", "<cmd>execute 'e ' .. expand('%:p:h')<CR>", desc = "Lir File Explorer"}
+        }
+    },
+    { "tamago324/lir-git-status.nvim", dependencies = "tamago324/lir.nvim" },
 }
