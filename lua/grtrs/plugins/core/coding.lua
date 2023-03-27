b9b6e627d3103056799530d42f72ed585dff84b0
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-03-27 15:02:47 +0200

    refactor(luasnip): move away from dependencies

--- lua/grtrs/plugins/core/coding.lua
+++ lua/grtrs/plugins/core/coding.lua
@@ -12,27 +12,18 @@ return {
         },
         lazy = true,
     },
-    -- Plugin: Autocompletion
-    { "hrsh7th/nvim-cmp",
-        event = { "InsertEnter", "CmdlineEnter" },
-        version = false, -- last release is way too old
+      -- Plugin: Snippets
+    {
+        "L3MON4D3/LuaSnip",
         dependencies = {
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "saadparwaiz1/cmp_luasnip",
-            "hrsh7th/cmp-nvim-lua",
-
-            -- Snippets
-            "L3MON4D3/LuaSnip",
             "rafamadriz/friendly-snippets",
+            config = function()
+                require("luasnip.loaders.from_vscode").lazy_load()
+                require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
+            end,
         },
-        opts = function()
-            local cmp = require("cmp")
-
+        init = function()
             local luasnip = require("luasnip")
-
-            require("luasnip.loaders.from_vscode").lazy_load()
-            require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" }})
 
             vim.keymap.set({ "i", "s" }, "<C-j>", function()
                 if luasnip.choice_active() then
@@ -42,9 +33,26 @@ return {
 
             vim.keymap.set({ "i", "s" }, "<C-k>", function()
                 if luasnip.choice_active() then
-                    luasnip.change_choice( -1)
+                    luasnip.change_choice(-1)
                 end
             end)
+
+        end,
+    },
+    -- Plugin: Autocompletion
+    {
+        "hrsh7th/nvim-cmp",
+        event = { "InsertEnter", "CmdlineEnter" },
+        version = false, -- last release is way too old
+        dependencies = {
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-nvim-lua",
+        },
+        opts = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
 
             local icons = require("grtrs.icons")
 
@@ -56,7 +64,7 @@ return {
                     end,
                 },
                 mapping = cmp.mapping.preset.insert({
-                    ['<C-b>'] = cmp.mapping.scroll_docs( -4),
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
@@ -76,20 +84,20 @@ return {
                     ['<S-Tab>'] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_prev_item()
-                        elseif luasnip.jumpable( -1) then
-                            luasnip.jump( -1)
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
                         else
                             fallback()
                         end
                     end, { 'i', 's' }),
                 }),
                 sources = {
-                    { name = 'path' },
                     { name = 'nvim_lsp' },
                     { name = 'nvim_lsp_signature_help' },
                     { name = "copilot", },
-                    { name = 'buffer' },
                     { name = 'luasnip' },
+                    { name = 'buffer' },
+                    { name = 'path' },
                 },
                 formatting = {
                     kind_icons = icons.kind,
