def66b9837932fe7423fcd901ac7019417d45116
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-03-28 12:07:35 +0200

    fix(align): add modes for normal and visual

--- lua/grtrs/plugins/core/editor.lua
+++ lua/grtrs/plugins/core/editor.lua
@@ -218,10 +218,10 @@ return {
         "Vonr/align.nvim",
         lazy = true,
         keys = {
-            { "aa", "<cmd>lua require('align').align_to_char(1, true)<CR>",           desc = "Align 1 character" },    -- Aligns to 1 character, looking left
-            { "as", "<cmd>lua require('align').align_to_char(2, true, true)<CR>",     desc = "Align 2 characters" },   -- Aligns to 2 characters, looking left and with previews
-            { "aw", "<cmd>lua require('align').align_to_char(false, true, true)<CR>", desc = "Aligns to string" },     -- Aligns to a string, looking left and with previews
-            { "ar", "<cmd>lua require('align').align_to_char(true, true, true)<CR>",  desc = "Align to Lua pattern" }, -- Aligns to a Lua pattern, looking left and with previews
+            { "aa", "<cmd>lua require('align').align_to_char(1, true)<CR>",           desc = "Align 1 character", mode = {"n", "v"} },    -- Aligns to 1 character, looking left
+            { "as", "<cmd>lua require('align').align_to_char(2, true, true)<CR>",     desc = "Align 2 characters", mode = {"n", "v"} },   -- Aligns to 2 characters, looking left and with previews
+            { "aw", "<cmd>lua require('align').align_to_char(false, true, true)<CR>", desc = "Aligns to string", mode = {"n", "v"} },     -- Aligns to a string, looking left and with previews
+            { "ar", "<cmd>lua require('align').align_to_char(true, true, true)<CR>",  desc = "Align to Lua pattern", mode = {"n", "v"} }, -- Aligns to a Lua pattern, looking left and with previews
         },
     },
     -- Plugin: Autopair
