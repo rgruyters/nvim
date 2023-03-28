516b109cff9169c71230ebcc574e165c1d9d4d6e
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-03-28 20:46:55 +0200

    chore(functions): remove unused functions

--- lua/grtrs/functions.lua
+++ lua/grtrs/functions.lua
@@ -1,17 +1,5 @@
 local M = {}
 
-M.nmap = function(keys, func, desc)
-    vim.keymap.set('n', keys, func, { noremap = true, silent = true, desc = desc })
-end
-
-M.imap = function(keys, func, desc)
-    vim.keymap.set('i', keys, func, { noremap = true, silent = true, desc = desc })
-end
-
-M.xmap = function(keys, func, desc)
-    vim.keymap.set('x', keys, func, { noremap = true, silent = true, desc = desc })
-end
-
 -- Borrowed from Lazyvim
 ---@param on_attach fun(client, buffer)
 function M.on_attach(on_attach)
