95b05a476d141abacd9e6d747898316fdd528692
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-03-28 10:50:18 +0200

    fix(lsp): enable automatic setup handling

--- lua/grtrs/lsp/init.lua
+++ lua/grtrs/lsp/init.lua
@@ -8,16 +8,6 @@ if not mason_lsp_loaded then
     return
 end
 
-local servers = {
-    "lua_ls",
-    "pyright",
-    "bashls",
-    "jsonls",
-    "yamlls",
-    "terraformls",
-    "tflint",
-}
-
 local settings = {
     log_level = vim.log.levels.INFO,
     max_concurrent_installers = 4,
@@ -26,32 +16,24 @@ local settings = {
 mason.setup(settings)
 
 mason_lspconfig.setup({
-    ensure_installed = servers,
+    ensure_installed = {},
     automatic_installation = true,
 })
 
-local lspconfig_loaded, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_loaded then
-    return
-end
+local opts = {
+    on_attach = require("grtrs.lsp.handlers").on_attach,
+    capabilities = require("grtrs.lsp.handlers").capabilities,
+}
 
-local opts = {}
-
-for _, server in pairs(servers) do
-    opts = {
-        on_attach = require("grtrs.lsp.handlers").on_attach,
-        capabilities = require("grtrs.lsp.handlers").capabilities,
-    }
-
-    server = vim.split(server, "@")[1]
-
-    local conf_opts_loaded, conf_opts = pcall(require, "grtrs.lsp.settings." .. server)
-    if conf_opts_loaded then
-        opts = vim.tbl_deep_extend("force", conf_opts, opts)
+mason_lspconfig.setup_handlers {
+    function(server)
+        local conf_opts_loaded, conf_opts = pcall(require, "grtrs.lsp.settings." .. server)
+        if conf_opts_loaded then
+            opts = vim.tbl_deep_extend("force", conf_opts, opts)
+        end
+        require("lspconfig")[server].setup(opts)
     end
-
-    lspconfig[server].setup(opts)
-end
+}
 
 require("grtrs.lsp.handlers").setup()
 require("grtrs.lsp.null-ls")
