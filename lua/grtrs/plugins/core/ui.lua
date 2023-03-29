4125d18df69e192e36be4cdbb702bf779badce56
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-03-29 20:55:41 +0200

    chore(webdev-icons): use overwrite by filename

--- lua/grtrs/plugins/core/ui.lua
+++ lua/grtrs/plugins/core/ui.lua
@@ -243,68 +243,62 @@ return {
     -- Plugin: Web devicons
     {
         "kyazdani42/nvim-web-devicons",
+        event = { "BufReadPost", "BufNewFile" },
         opts = {
-            sh = {
-                icon = "",
-                color = "#1DC123",
-                cterm_color = "59",
-                name = "Sh",
-            },
-            [".gitattributes"] = {
-                icon = "",
-                color = "#e24329",
-                cterm_color = "59",
-                name = "GitAttributes",
-            },
-            [".gitconfig"] = {
-                icon = "",
-                color = "#e24329",
-                cterm_color = "59",
-                name = "GitConfig",
-            },
-            [".gitignore"] = {
-                icon = "",
-                color = "#e24329",
-                cterm_color = "59",
-                name = "GitIgnore",
-            },
-            [".gitlab-ci.yml"] = {
-                icon = "",
-                color = "#e24329",
-                cterm_color = "166",
-                name = "GitlabCI",
-            },
-            [".gitmodules"] = {
-                icon = "",
-                color = "#e24329",
-                cterm_color = "59",
-                name = "GitModules",
-            },
-            ["diff"] = {
-                icon = "",
-                color = "#e24329",
-                cterm_color = "59",
-                name = "Diff",
-            },
-            ["pp"] = {
-                icon = "",
-                color = "#FFA61A",
-                name = "Pp",
-            },
-            ["epp"] = {
-                icon = "",
-                color = "#FFA61A",
-                name = "Epp",
-            },
-            ["tf"] = {
-                icon = "󱁢",
-                color = "#5F43E9",
-                cterm_color = "57",
-                name = "Terraform",
+            override_by_filename = {
+                [".gitattributes"] = {
+                    icon = "",
+                    color = "#e24329",
+                    cterm_color = "59",
+                    name = "GitAttributes",
+                },
+                [".gitconfig"] = {
+                    icon = "",
+                    color = "#e24329",
+                    cterm_color = "59",
+                    name = "GitConfig",
+                },
+                [".gitignore"] = {
+                    icon = "",
+                    color = "#e24329",
+                    cterm_color = "59",
+                    name = "GitIgnore",
+                },
+                [".gitlab-ci.yml"] = {
+                    icon = "",
+                    color = "#e24329",
+                    cterm_color = "166",
+                    name = "GitlabCI",
+                },
+                [".gitmodules"] = {
+                    icon = "",
+                    color = "#e24329",
+                    cterm_color = "59",
+                    name = "GitModules",
+                },
+                ["diff"] = {
+                    icon = "",
+                    color = "#e24329",
+                    cterm_color = "59",
+                    name = "Diff",
+                },
+                ["pp"] = {
+                    icon = "",
+                    color = "#FFA61A",
+                    name = "Pp",
+                },
+                ["epp"] = {
+                    icon = "",
+                    color = "#FFA61A",
+                    name = "Epp",
+                },
+                ["tf"] = {
+                    icon = "󱁢",
+                    color = "#5F43E9",
+                    cterm_color = "57",
+                    name = "Terraform",
+                },
             },
         },
-        config = function(_,opts)
-            require("nvim-web-devicons").set_icon(opts)
-        end,
     },
 }
