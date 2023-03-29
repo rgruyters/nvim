ead2df0a54ff132d5c9f2a5fb672ca7a23eb42a8
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-03-29 14:13:04 +0200

    perf(luasnip): lazyload based on events

--- lua/grtrs/plugins/core/coding.lua
+++ lua/grtrs/plugins/core/coding.lua
@@ -13,6 +13,7 @@ return {
     -- Plugin: Snippets
     {
         "L3MON4D3/LuaSnip",
+        event = { "InsertEnter", "CmdlineEnter" },
         dependencies = {
             "rafamadriz/friendly-snippets",
             config = function()
