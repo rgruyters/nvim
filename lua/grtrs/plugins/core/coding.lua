4c4d1c4a1e73ca618b26d0234e0b1a62c441f0f7
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-03-29 17:35:15 +0200

    feat(nvim-cmp): disable completion in comments
    This can sometimes be annoying when you type a comment and completion
    tries to guess (complete, red.) your comments

--- lua/grtrs/plugins/core/coding.lua
+++ lua/grtrs/plugins/core/coding.lua
@@ -104,7 +104,19 @@ return {
 
                         return item
                     end,
-                }
+                },
+                enabled = function()
+                    -- disable completion in comments
+                    -- https://github.com/hrsh7th/nvim-cmp/wiki/Advanced-techniques#disabling-completion-in-certain-contexts-such-as-comments
+                    local context = require 'cmp.config.context'
+                    -- keep command mode completion enabled when cursor is in a comment
+                    if vim.api.nvim_get_mode().mode == 'c' then
+                        return true
+                    else
+                        return not context.in_treesitter_capture("comment")
+                            and not context.in_syntax_group("Comment")
+                    end
+                end,
             }
         end
     },
