a6e879d935ea87ad558e997fd597160f32a01da8
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-08-01 14:48:50 +0200

    fix(align): only map for visual mode

--- lua/custom/plugins/align.lua
+++ lua/custom/plugins/align.lua
@@ -4,13 +4,13 @@ return {
     "Vonr/align.nvim",
     keys = {
       -- Aligns to 1 character, looking left
-      { "aa", "<cmd>lua require('align').align_to_char(1, true)<CR>",           desc = "Align 1 character", mode = {"n", "v"} },
+      { "aa", "<cmd>lua require('align').align_to_char(1, true)<CR>",           desc = "Align 1 character", mode = {"v"} },
       -- Aligns to 2 characters, looking left and with previews
-      { "as", "<cmd>lua require('align').align_to_char(2, true, true)<CR>",     desc = "Align 2 characters", mode = {"n", "v"} },
+      { "as", "<cmd>lua require('align').align_to_char(2, true, true)<CR>",     desc = "Align 2 characters", mode = {"v"} },
       -- Aligns to a string, looking left and with previews
-      { "aw", "<cmd>lua require('align').align_to_char(false, true, true)<CR>", desc = "Aligns to string", mode = {"n", "v"} },
+      { "aw", "<cmd>lua require('align').align_to_char(false, true, true)<CR>", desc = "Aligns to string", mode = {"v"} },
       -- Aligns to a Lua pattern, looking left and with previews
-      { "ar", "<cmd>lua require('align').align_to_char(true, true, true)<CR>",  desc = "Align to Lua pattern", mode = {"n", "v"} },
+      { "ar", "<cmd>lua require('align').align_to_char(true, true, true)<CR>",  desc = "Align to Lua pattern", mode = {"v"} },
     },
   },
 }
