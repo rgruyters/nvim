45d6509c6b7d54cdb11b88a23c0242dd32bad560
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-08-01 12:09:27 +0200

    fix(tree-sitter): use git as preferred method

--- lua/kickstart/core.lua
+++ lua/kickstart/core.lua
@@ -450,7 +450,8 @@ return {
       },
     },
     config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+      require('nvim-treesitter.configs').setup(opts)
+      require('nvim-treesitter.install').prefer_git = true
     end,
   },
 }
