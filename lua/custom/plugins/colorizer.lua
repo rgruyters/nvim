e2dbb88af4ee44282c39e2874c909901f9ac8429
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-08-02 08:28:38 +0200

    chore(lazy): add colorizer plugin

---
+++ lua/custom/plugins/colorizer.lua
@@ -0,0 +1,10 @@
+return {
+    -- Plugin: Show colour codes
+    {
+        'NvChad/nvim-colorizer.lua',
+        event = 'BufRead',
+    },
+}
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
