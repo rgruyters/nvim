58a0a19544f8f9d8c4b63ab97c6e7c3f793a6ff1
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-08-02 07:41:32 +0200

    chore(lsp): add schemastore settings for yamlls

---
+++ lua/custom/lsp/settings/yamlls.lua
@@ -0,0 +1,10 @@
+return {
+    settings = {
+        yaml = {
+            schemaStore = {
+                enable = true,
+                url = "https://www.schemastore.org/api/json/catalog.json",
+            },
+        }
+    }
+}
