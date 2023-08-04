379aa5455430b25aa8468a1ccb4f674bc1570b4c
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-08-04 20:19:08 +0200

    feat(lazy): add treesitter-context plugin

---
+++ lua/custom/plugins/treesitter-context.lua
@@ -0,0 +1,13 @@
+return {
+  'nvim-treesitter/nvim-treesitter-context',
+  event = { 'BufReadPost', 'BufNewFile' },
+  dependencies = 'nvim-treesitter/nvim-treesitter',
+  opts = {
+    enable = true,   -- Enable this plugin (Can be enabled/disabled later via commands)
+    throttle = true, -- Throttles plugin updates (may improve performance)
+    max_lines = 0,   -- How many lines the window should span. Values <= 0 mean no limit.
+  },
+}
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
