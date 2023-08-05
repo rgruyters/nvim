8675690334f28d92a7b203d2c564821b55982949
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-08-05 09:45:55 +0200

    chore(copilot): remove separator

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -33,6 +33,7 @@ return {
           local ok, clients = pcall(vim.lsp.get_active_clients, { name = "copilot", bufnr = 0 })
           return ok and #clients > 0
         end,
+        separator = "%#SLSeparator#" .. "" .. "%*",
       })
     end,
   },
