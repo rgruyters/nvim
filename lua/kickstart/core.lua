f1e261d37ef26e2ee26ecef6e544bdfadbaa2646
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-08-07 09:33:36 +0200

    chore(lazy): use event state for lspconfig plugin

--- lua/kickstart/core.lua
+++ lua/kickstart/core.lua
@@ -24,7 +24,7 @@ return {
   {
     -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
-    lazy = true,
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
       { 'williamboman/mason.nvim', config = true },
