80a02c582df15e2c810cf9da5fbb9c466ea71397
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-08-09 17:52:19 +0200

    docs(comments): minor comments updates

--- init.lua
+++ init.lua
@@ -5,8 +5,8 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Install package manager
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
+-- https://github.com/folke/lazy.nvim
+-- `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
@@ -21,10 +21,10 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- NOTE: Here is where you install your plugins.
---  You can configure plugins using the `config` key.
+--       You can configure plugins using the `config` key.
 --
 --  You can also configure plugins after the setup call,
---    as they will be available in your neovim runtime.
+--  as they will be available in your neovim runtime.
 require('lazy').setup({
   install = {
     colorscheme = { "catppuccin" }
@@ -44,15 +44,16 @@ require('lazy').setup({
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
+  --
   -- require 'kickstart.plugins.autoformat',
   require 'kickstart.plugins.debug',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+  --       You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
+  --       up-to-date with whatever is in the kickstart repo.
+  --       For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
+  --       Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   { import = 'custom.plugins' },
 }, {})
 
--- lua/kickstart/core.lua
+++ lua/kickstart/core.lua
@@ -1,5 +1,4 @@
 return {
-  -- NOTE: First, some plugins that don't require any configuration
   -- Git related plugins
   {
     'tpope/vim-fugitive',
@@ -20,7 +19,7 @@ return {
   -- 'tpope/vim-sleuth',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
-  --  The configuration is done below. Search for lspconfig to find it below.
+  --       The configuration is done in lua/kickstart/lsp.lua.
   {
     -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
