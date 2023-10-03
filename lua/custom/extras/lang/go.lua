93ab1bbfeb89ff92c657a09ebfeed6e36b27a1c0
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-10-03 11:19:13 +0200

    feat(go): add golang language support

---
+++ lua/custom/extras/lang/go.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        'go',
+      })
+    end,
+  },
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = true,
+            },
+          },
+          flags = {
+            debounce_text_changes = 150,
+          },
+        },
+      },
+    },
+  },
+}
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
