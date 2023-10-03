124be584e6cdad479037dbbe24e6175a92de51e4
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-10-03 12:35:14 +0200

    chore(yaml): ensure yaml installed for treesitter

--- lua/custom/extras/lang/yaml.lua
+++ lua/custom/extras/lang/yaml.lua
@@ -1,5 +1,13 @@
 return {
   {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        'yaml',
+      })
+    end,
+  },
+  {
     'neovim/nvim-lspconfig',
     opts = {
       servers = {
