dd7fb86fb6d69a84642ae0869c9ade47eb678735
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-10-04 09:32:30 +0200

    feat(lang): add helm support

---
+++ lua/custom/extras/lang/helm.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    'towolf/vim-helm',
+    ft = 'helm',
+  },
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        helm = {},
+        yamlls = {
+          filetypes_exclude = { 'helm' },
+        },
+      },
+    },
+    setup = {
+      yamlls = function(_, opts)
+        local yaml = require('lspconfig.server_configurations.yamlls')
+        -- @param ft string
+        opts.filetypes = vim.tbl_filter(function(ft)
+          return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+        end, yaml.default_config.filetypes)
+      end,
+    }
+  },
+}
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
