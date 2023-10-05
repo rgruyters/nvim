e0d0ffc25c17f9ad0be682107a8d2134adcbc047
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-10-05 15:03:37 +0200

    refactor(python): move python dap related to language file

--- lua/custom/extras/lang/python.lua
+++ lua/custom/extras/lang/python.lua
@@ -47,6 +47,56 @@ return {
       end
     end,
   },
+  {
+    -- use own debugging language
+    'mfussenegger/nvim-dap-python',
+    dependencies = { 'mfussenegger/nvim-dap' },
+    ft = 'python',
+    config = function()
+      local dap_python = require('dap-python')
+      local mason_path = vim.fn.glob(vim.fn.stdpath 'data' .. '/mason/')
+
+      dap_python.setup(mason_path .. 'packages/debugpy/venv/bin/python')
+
+      dap_python.test_runner = 'pytest'
+    end,
+  },
+  {
+    'nvim-neotest/neotest-python',
+    ft = 'python',
+    dependencies = {
+      'nvim-neotest/neotest',
+    },
+    keys = {
+      { '<leader>tm', "<cmd>lua require('neotest').run.run()<cr>",
+        { desc = 'Test Method' }
+      },
+      { '<leader>tM', "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>",
+        { desc = 'Test Method DAP' }
+      },
+      { '<leader>tf', "<cmd>lua require('neotest').run.run({vim.fn.expand('%')})<cr>",
+        { desc = 'Test Class' }
+      },
+      { '<leader>tF', "<cmd>lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>",
+        { desc = 'Test Class DAP' }
+      },
+      { '<leader>tS', "<cmd>lua require('neotest').summary.toggle()<cr>",
+        { desc = 'Test Summary' }
+      },
+    },
+    opts = {
+      adapters = {
+        ["neotest-python"] = {
+          dap = {
+            justMyCode = false,
+            console = 'integratedTerminal',
+          },
+          args = { '--log-level', 'DEBUG', '--quiet' },
+          runner = 'pytest',
+        },
+      },
+    },
+  },
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
