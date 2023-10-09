ae35fc1948d15ca4d82931548029c120f0c1e9af
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-10-09 16:48:50 +0200

    chore(neotest): load custom adapters and setups
    Ability to inject custom adapters into neotest. Thanks to lazyvim
    project.

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -4,6 +4,36 @@ return {
     opts = {
       adapters = {},
     },
+    config = function(_, opts)
+      -- if adapters option is set, load it!
+      if opts.adapters then
+        local adapters = {}
+        for name, config in pairs(opts.adapters or {}) do
+          if type(name) == 'number' then
+            if type(config) == 'string' then
+              config = require(config)
+            end
+            adapters[#adapters + 1] = config
+          elseif config ~= false then
+            local adapter = require(name)
+            if type(config) == 'table' and not vim.tbl_isempty(config) then
+              local meta = getmetatable(adapter)
+              if adapter.setup then
+                adapter.setup(config)
+              elseif meta and meta.__call then
+                adapter(config)
+              else
+                error('Adapter ' .. name .. ' does not support setup')
+              end
+            end
+            adapters[#adapters + 1] = adapter
+          end
+        end
+        opts.adapters = adapters
+      end
+
+      require('neotest').setup(opts)
+    end,
     keys = {
       { '<leader>tm', "<cmd>lua require('neotest').run.run()<cr>",
         { desc = 'Test Method' }
