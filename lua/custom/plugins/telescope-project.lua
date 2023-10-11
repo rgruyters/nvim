dfe09c06c95bbad12b5d19d7b9db470de227929a
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-10-11 10:40:59 +0200

    chore(project): reduce opts and load on very lazy

--- lua/custom/plugins/telescope-project.lua
+++ lua/custom/plugins/telescope-project.lua
@@ -2,21 +2,13 @@ return {
   'telescope.nvim',
   dependencies = {
     'ahmedkhalf/project.nvim',
-    event = 'VimEnter',
+    event = "VeryLazy",
     cmd = 'Telescope projects',
     keys = {
       { '<leader>fp', '<cmd>Telescope projects<CR>', desc = '[F]ind [P]rojects' },
     },
     opts = {
-      active = true,
-      on_config_done = nil,
-      manual_mode = false,
       detection_methods = { 'pattern' },
-      patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json' },
-      show_hidden = true,
-      silent_chdir = true,
-      ignore_lsp = {},
-      datapath = vim.fn.stdpath('data'),
     },
     config = function(_, opts)
       require('project_nvim').setup(opts)
