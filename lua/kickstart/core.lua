d011b6c78e86f19afe335427f5fad1ad656e5811
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-10-27 10:45:34 +0200

    chore(cmp): make next/prev consistent

--- lua/kickstart/core.lua
+++ lua/kickstart/core.lua
@@ -48,8 +48,8 @@ return {
           end,
         },
         mapping = cmp.mapping.preset.insert {
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete {},
