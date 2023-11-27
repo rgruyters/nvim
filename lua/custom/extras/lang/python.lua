190ce2d0470d49a9b643d160a591288b83d9a323
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-11-27 16:45:50 +0100

    chore(python): add ruff-lsp settings

--- lua/custom/extras/lang/python.lua
+++ lua/custom/extras/lang/python.lua
@@ -29,9 +29,20 @@ return {
                 },
               },
             },
+            ruff_lsp = {},
           },
         },
       },
+    },
+    setup = {
+      ruff_lsp = function()
+        require('custom.functions').on_attach(function(client, _)
+          if client.name == "ruff_lsp" then
+            -- Disable hover in favor of Pyright
+            client.server_capabilities.hoverProvider = false
+          end
+        end)
+      end,
     },
   },
   {
