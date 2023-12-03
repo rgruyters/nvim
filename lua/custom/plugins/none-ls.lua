86ef211ab7c123a1b3968967e8bae1661863d652
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-12-03 14:42:06 +0100

    feat(lsp): install lsps on filetype
    Install lsp, formats or linters when opening supported filetype

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -3,21 +3,24 @@ return {
   {
     'nvimtools/none-ls.nvim',
     event = 'LspAttach',
-    opts = function()
-      local nonels = require('null-ls')
-
-      return {
-        sources = {
-          nonels.builtins.formatting.prettier.with {
-            extra_filetypes = { 'toml', 'markdown' },
-            extra_args = { '--no-semi', '--single-quote', '--jsx-single-quote' },
-          },
-          nonels.builtins.formatting.shfmt,
-          nonels.builtins.formatting.stylua,
-          nonels.builtins.diagnostics.markdownlint,
-        },
-      }
-    end
+    opts = function(_, opts)
+      local nls = require('null-ls')
+      opts.root_dir = opts.root_dir
+        or require('null-ls.utils').root_pattern('.null-ls-root', '.neoconf.json', 'Makefile', '.git')
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.formatting.stylua,
+        nls.builtins.formatting.shfmt,
+        nls.builtins.diagnostics.markdownlint,
+      })
+    end,
+  },
+  -- Load required formatters and linters
+  {
+    'mason.nvim',
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { 'stylua', 'shfmt', 'markdownlint-cli2' })
+    end,
   },
 }
 
