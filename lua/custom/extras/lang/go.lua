2be465a836b07c7e20e01e7a4c5d7216c870fcf8
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-12-04 17:23:55 +0100

    chore(lang): add lsps for Go

--- lua/custom/extras/lang/go.lua
+++ lua/custom/extras/lang/go.lua
@@ -7,6 +7,14 @@ return {
       })
     end,
   },
+  -- Ensure Go tools are installed
+  {
+    'williamboman/mason.nvim',
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { 'goimports', 'gofumpt' })
+    end,
+  },
   {
     'neovim/nvim-lspconfig',
     opts = {
