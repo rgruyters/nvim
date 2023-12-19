b6894984d835dfc2eec56713bcf1356c580b5578
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-12-19 15:44:23 +0100

    chore(colorscheme): use darker colorscheme

--- lua/kickstart/core.lua
+++ lua/kickstart/core.lua
@@ -227,7 +227,7 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'catppuccin-macchiato'
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 
