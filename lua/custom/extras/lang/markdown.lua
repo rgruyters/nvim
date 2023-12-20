3f55ca177a75a051aa9fef6b2e8c2477d01d5681
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-12-20 09:58:46 +0100

    chore(lang): add markdown filetypes

---
+++ lua/custom/extras/lang/markdown.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == 'table' then
+        vim.list_extend(opts.ensure_installed, { 'markdown', 'markdown_inline' })
+      end
+    end,
+  },
+  {
+    'williamboman/mason.nvim',
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { 'markdownlint' })
+    end,
+  },
+  {
+    'mfussenegger/nvim-lint',
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        markdown = { 'markdownlint' },
+      },
+    },
+  },
+}
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
