5311a44be2d7980b89d4661a0caf22f92b0803e0
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-12-21 15:28:32 +0100

    chore(lang): add lsp info for TypeScript

--- lua/custom/extras/lang/typescript.lua
+++ lua/custom/extras/lang/typescript.lua
@@ -8,6 +8,44 @@ return {
       })
     end,
   },
+  -- Ensure Typescript tools are installed
+  {
+    'williamboman/mason.nvim',
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { 'typescript-language-server' })
+    end,
+  },
+  -- correctly setup lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        tsserver = {
+          settings = {
+            typescript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            javascript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            completions = {
+              completeFunctionCalls = true,
+            },
+          },
+        },
+      },
+    },
+  },
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
