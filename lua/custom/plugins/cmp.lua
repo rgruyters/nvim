f5c77eb6ce5783a433348d2c646e39336abb7d58
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-12-28 16:52:55 +0100

    chore(cmp): remove buffer completion

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -2,17 +2,12 @@ return {
   'nvim-cmp',
   event = 'InsertEnter',
   dependencies = {
-    'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-nvim-lsp-signature-help',
     'saadparwaiz1/cmp_luasnip',
   },
   opts = function(_, opts)
     table.insert(opts.sources, 1, {
       name = 'nvim_lsp_signature_help',
-    })
-
-    table.insert(opts.sources, 1, {
-      name = 'buffer',
     })
   end,
 }
