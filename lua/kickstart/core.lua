ebac679bca764a5cbe3448b8eccdef36eb6d15cf
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-01-03 11:45:02 +0100

    refactor(telescope): move keymaps to config
    Make it more readable and do not call `require('telescope.builtin')` multiple times

--- lua/kickstart/core.lua
+++ lua/kickstart/core.lua
@@ -345,89 +345,6 @@ return {
     branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
     -- See `:help telescope.builtin`
-    keys = {
-      {
-        '<leader>?',
-        function()
-          require('telescope.builtin').oldfiles()
-        end,
-        desc = '[?] Find recently opened files',
-      },
-      {
-        '<leader><space>',
-        function()
-          require('telescope.builtin').buffers()
-        end,
-        desc = '[ ] Find existing buffers',
-      },
-      {
-        '<leader>/',
-        function()
-          -- You can pass additional configuration to telescope to change theme, layout, etc.
-          require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
-            winblend = 10,
-            previewer = false,
-          }))
-        end,
-        desc = '[/] Fuzzily search in current buffer',
-      },
-      {
-        '<leader>gf',
-        function()
-          require('telescope.builtin').git_files()
-        end,
-        desc = '[G]it [F]iles',
-      },
-      {
-        '<leader>ff',
-        function()
-          require('telescope.builtin').find_files()
-        end,
-        desc = '[F]ind [F]iles',
-      },
-      {
-        '<leader>fh',
-        function()
-          require('telescope.builtin').help_tags()
-        end,
-        desc = '[F]ind [H]elp',
-      },
-      {
-        '<leader>fw',
-        function()
-          require('telescope.builtin').grep_string()
-        end,
-        desc = '[F]ind current [W]ord',
-      },
-      {
-        '<leader>fg',
-        function()
-          require('telescope.builtin').live_grep()
-        end,
-        desc = '[F]ind by [G]rep',
-      },
-      {
-        '<leader>fd',
-        function()
-          require('telescope.builtin').diagnostics()
-        end,
-        desc = '[F]ind [D]iagnostics',
-      },
-      {
-        '<leader>gc',
-        function()
-          require('telescope.builtin').git_commits()
-        end,
-        desc = '[G]it [C]ommits',
-      },
-      {
-        '<leader>fk',
-        function()
-          require('telescope.builtin').keymaps()
-        end,
-        desc = '[F]ind [K]eymaps',
-      },
-    },
     opts = function()
       local actions = require('telescope.actions')
 
@@ -469,6 +386,21 @@ return {
       }
     end,
     config = function(_, opts)
+      local builtin = require('telescope.builtin')
+
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+      vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+      vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer' })
+      vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = '[G]it [F]iles' })
+      vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = '[G]it [B]ranches' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
+      vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
+      vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
+      vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = '[G]it [C]ommits' })
+      vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[F]ind [K]eymaps' })
+
       require('telescope').setup(opts)
     end,
   },
