b54c36959ac5c7c6f68674dc63a9e68a0eabefe5
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-03-06 17:32:45 +0100

    chore(custom): update custom options

--- init.lua
+++ init.lua
@@ -148,6 +148,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- [[ Custom Options ]]
+require('custom.options')
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
