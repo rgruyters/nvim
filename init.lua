55fe4af1b9eb1fe8890deb4556d38c65904bad98
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-03-06 17:03:37 +0100

    fix(cmp): use Enter (CR) for completion

--- init.lua
+++ init.lua
@@ -746,10 +746,10 @@ require('lazy').setup({
           -- Select the [p]revious item
           ['<C-p>'] = cmp.mapping.select_prev_item(),
 
-          -- Accept ([y]es) the completion.
+          -- Accept the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
