e3b04eb91f8654e42fabd4226c3ca4fdc7adce37
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-03-06 17:39:23 +0100

    fix(snippets): make snippets work

--- lua/kickstart/plugins/friendly-snippets.lua
+++ lua/kickstart/plugins/friendly-snippets.lua
@@ -1,10 +1,12 @@
 return {
-  'rafamadriz/friendly-snippets',
-  event = 'InsertEnter',
-  dependencies = { 'L3MON4D3/LuaSnip' },
-  config = function()
-    local lsvs = require('luasnip.loaders.from_vscode')
-    lsvs.lazy_load()
-    lsvs.lazy_load({ paths = { './snippets' } })
-  end,
+  'L3MON4D3/LuaSnip',
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    event = 'InsertEnter',
+    config = function()
+      local lsvs = require('luasnip.loaders.from_vscode')
+      lsvs.lazy_load()
+      lsvs.lazy_load({ paths = { './snippets' } })
+    end,
+  },
 }
