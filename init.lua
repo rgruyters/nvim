054359931a9e04b22c6c2289f4ecc16bf956e878
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-03-11 09:02:42 +0100

    fix(mason): remove event handling for lsp

--- init.lua
+++ init.lua
@@ -466,7 +466,6 @@ require('lazy').setup({
 
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
-    event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for neovim
       'williamboman/mason.nvim',
