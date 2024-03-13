fd374acd9dc4b03990e19370ad807c2498ec36b3
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-03-13 09:24:44 +0100

    chore(python): add missing black formatter

--- lua/custom/extras/lang/python.lua
+++ lua/custom/extras/lang/python.lua
@@ -46,6 +46,13 @@ return {
     },
   },
   {
+    'williamboman/mason.nvim',
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      table.insert(opts.ensure_installed, 'black')
+    end,
+  },
+  {
     'stevearc/conform.nvim',
     optional = true,
     opts = {
