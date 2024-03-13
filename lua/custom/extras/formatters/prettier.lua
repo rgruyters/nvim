50c11be66efdbc0b9732cc3fa4bb1eed026b674b
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-03-13 09:24:44 +0100

    chore(python): add missing black formatter

--- lua/custom/extras/formatters/prettier.lua
+++ lua/custom/extras/formatters/prettier.lua
@@ -2,6 +2,7 @@ return {
   {
     'williamboman/mason.nvim',
     opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
       table.insert(opts.ensure_installed, 'prettier')
     end,
   },
