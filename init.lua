3b5af75b841beaeab1b0605a46738d895abf0046
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-03-21 13:04:57 +0100

    refactor(init): use rose-pine as rose-pine

--- init.lua
+++ init.lua
@@ -831,6 +831,7 @@ require('lazy').setup({
     'rose-pine/neovim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
+    name = 'rose-pine',
     opts = {
       dark_variant = 'moon',
 
