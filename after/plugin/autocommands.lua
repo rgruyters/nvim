1a188f6643e90180ec945f93c33f78a22c6ab778
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-03-21 13:00:28 +0100

    chore(autocommands): remove dupped autocmd for yank highlight

--- after/plugin/autocommands.lua
+++ after/plugin/autocommands.lua
@@ -16,16 +16,6 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
   end,
 })
 
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
-
 -- Add project name to titlebar of terminal
 vim.api.nvim_create_autocmd({ 'BufEnter' }, {
   pattern = { '' },
