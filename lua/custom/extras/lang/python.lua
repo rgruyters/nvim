974c22f4e6772b10c2ae46ef82f0d2571873bb27
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-03-27 14:22:00 +0100

    fix(python): use standard type checking

--- lua/custom/extras/lang/python.lua
+++ lua/custom/extras/lang/python.lua
@@ -15,6 +15,7 @@ return {
           settings = {
             basedpyright = {
               disableOrganizeImports = false,
+              typeCheckingMode = 'standard',
             },
             python = {
               analysis = {
