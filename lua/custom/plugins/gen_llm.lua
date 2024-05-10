c337d48098d55c8e882eeaf7ac43ad0d98e50bbb
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-05-10 12:56:32 +0200

    chore(gen.nvim): use mistral LLM and custom prompt

--- lua/custom/plugins/gen_llm.lua
+++ lua/custom/plugins/gen_llm.lua
@@ -1,6 +1,14 @@
 return {
   'David-Kunz/gen.nvim',
   opts = {
-    model = 'llama3',
+    model = 'mistral',
   },
+  config = function(_, opts)
+    local gen = require('gen')
+    gen.setup(opts)
+    gen.prompts['Work_With_Me'] = {
+      prompt = 'You are a senior devops engineer, acting as an assistant. You offer help with cloud technologies like: Terraform, AWS, kubernetes, python. You answer with code examples when possible. $input:\n$text',
+      replace = true,
+    }
+  end,
 }
