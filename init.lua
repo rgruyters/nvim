e412536c4658a43ad8d7767c04a455f8c497916b
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-05-22 17:03:43 +0200

    fix(init): use event.buf for inlayhints

--- init.lua
+++ init.lua
@@ -622,8 +622,8 @@ require('lazy').setup({
           --
           -- This may be unwanted, since they displace some of your code
           if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map('n', '<leader>th', function(bufnr)
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr or 0 }))
+            map('n', '<leader>th', function()
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
             end, '[T]oggle Inlay [H]ints')
           end
         end,
