1aa23bd98991e4eba769efc936c582595cb0b899
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-05-29 09:39:16 +0200

    chore(nvim-0.10): use new plugins support
    Use more features from Neovim version 0.10

---
+++ lua/custom/plugins/ts-comments.lua
@@ -0,0 +1,10 @@
+-- enhanced builtin nvim comments
+return {
+  'folke/ts-comments.nvim',
+  event = 'VeryLazy',
+  opts = {},
+  enabled = vim.fn.has('nvim-0.10') == 1,
+}
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
