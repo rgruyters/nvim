2921190bbda241953944b307b359b1a5e9f79ceb
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-05-29 10:15:49 +0200

    chore(python): install mypy for linting

--- lua/custom/extras/lang/python.lua
+++ lua/custom/extras/lang/python.lua
@@ -8,6 +8,13 @@ return {
     end,
   },
   {
+    'williamboman/mason.nvim',
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { 'mypy' })
+    end,
+  },
+  {
     'neovim/nvim-lspconfig',
     opts = {
       servers = {
