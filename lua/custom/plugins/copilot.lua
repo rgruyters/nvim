1b0312ebb1eb84b2af0ceca58d4bca46a8a3ec84
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-05-29 09:42:19 +0200

    fix: replace deprecated functions

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -31,7 +31,7 @@ return {
           return icon .. (status.message or '') .. ' '
         end,
         cond = function()
-          local ok, clients = pcall(vim.lsp.get_active_clients, { name = 'copilot', bufnr = 0 })
+          local ok, clients = pcall(vim.lsp.get_clients, { name = 'copilot', bufnr = 0 })
           return ok and #clients > 0
         end,
         padding = { left = 1, right = 1 },
