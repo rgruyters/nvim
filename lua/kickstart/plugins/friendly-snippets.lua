f5e006c2f7761898cd196789876da9c9d0731d89
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-06-06 14:12:36 +0200

    revert(snippets): do not use nvim-snippets, yet

--- lua/kickstart/plugins/friendly-snippets.lua
+++ lua/kickstart/plugins/friendly-snippets.lua
@@ -1,38 +1,16 @@
-return vim.fn.has('nvim-0.10') == 1
-    and {
-      'nvim-cmp',
-      dependencies = {
-        'garymjr/nvim-snippets',
-        opts = {
-          friendly_snippets = true,
-        },
-        dependencies = {
-          'rafamadriz/friendly-snippets',
-          event = 'InsertEnter',
-          config = function()
-            local snippets = require('luasnip.loaders.from_vscode')
-            snippets.lazy_load()
-            snippets.lazy_load({ paths = { './snippets' } })
-          end,
-        },
-      },
-      opts = function(_, opts)
-        table.insert(opts.sources, { name = 'snippets' })
-      end,
-    }
-  or {
-    -- Add a bunch of pre-configured snippets. Custom snippets can be added to
-    -- the `snippets/` folder in the root of the project.
-    'L3MON4D3/LuaSnip',
-    event = { 'InsertEnter' },
-    dependencies = {
-      'rafamadriz/friendly-snippets',
-      event = 'InsertEnter',
-      config = function()
-        local snippets = require('luasnip.loaders.from_vscode')
-        snippets.lazy_load()
-        snippets.lazy_load({ paths = { './snippets' } })
-      end,
-    },
-    enabled = vim.fn.has('nvim-0.10') == 0,
-  }
+return {
+  -- Add a bunch of pre-configured snippets. Custom snippets can be added to
+  -- the `snippets/` folder in the root of the project.
+  'L3MON4D3/LuaSnip',
+  event = { 'InsertEnter' },
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    event = 'InsertEnter',
+    config = function()
+      local snippets = require('luasnip.loaders.from_vscode')
+      snippets.lazy_load()
+      snippets.lazy_load({ paths = { './snippets' } })
+    end,
+  },
+  enabled = vim.fn.has('nvim-0.10') == 0,
+}
