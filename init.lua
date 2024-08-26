0230e6cd25f3b0bf4957dd3884640e9ca057ca01
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-08-26 20:46:01 +0200

    chore(init): replace deprecated lsp_fallback for conform

--- init.lua
+++ init.lua
@@ -698,9 +698,15 @@ require('lazy').setup({
           -- have a well standardized coding style. You can add additional
           -- languages here or re-enable it for the disabled ones.
           local disable_filetypes = { c = true, cpp = true }
+          local lsp_format_opt
+          if disable_filetypes[vim.bo[bufnr].filetype] then
+            lsp_format_opt = 'never'
+          else
+            lsp_format_opt = 'fallback'
+          end
           return {
             timeout_ms = 500,
-            lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+            lsp_format = lsp_format_opt,
           }
         else
           return false
