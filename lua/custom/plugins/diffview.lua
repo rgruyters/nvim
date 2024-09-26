1a8a4ef36c862f9fe2ad865ce75c8d0464ee731a
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-09-26 10:06:28 +0200

    feat(custom): add diffview plugins

---
+++ lua/custom/plugins/diffview.lua
@@ -0,0 +1,7 @@
+return {
+  'sindrets/diffview.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+}
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
