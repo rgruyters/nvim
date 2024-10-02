d22ad71349a780374b4e8e8de99a0a6a2c5ffd0b
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-10-02 09:05:25 +0200

    chore(custom): use deepseek-coder model for LLM

--- lua/custom/plugins/gen_llm.lua
+++ lua/custom/plugins/gen_llm.lua
@@ -1,13 +1,14 @@
 return {
   'David-Kunz/gen.nvim',
+  event = 'InsertEnter',
   opts = {
-    model = 'mistral',
+    model = 'deepseek-coder',
   },
   config = function(_, opts)
     local gen = require('gen')
     gen.setup(opts)
     gen.prompts['Work_With_Me'] = {
-      prompt = 'You are a senior devops engineer, acting as an assistant. You offer help with cloud technologies like: Terraform, AWS, kubernetes, python. You answer with code examples when possible. $input:\n$text',
+      prompt = 'You are a senior devops engineer, acting as an assistant. You offer help with cloud technologies like: Terraform, AWS, Kubernetes, Python and Go. You answer with code examples when possible. $input:\n$text',
       replace = true,
     }
   end,
