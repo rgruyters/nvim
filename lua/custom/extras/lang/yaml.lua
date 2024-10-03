2fbc42aed55ad60cfe33ce21aa965aac9fe2b718
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-10-03 20:48:56 +0200

    chore(extras): support for external schemastore

--- lua/custom/extras/lang/yaml.lua
+++ lua/custom/extras/lang/yaml.lua
@@ -7,16 +7,47 @@ return {
       })
     end,
   },
+  -- yaml schema support
+  {
+    'b0o/SchemaStore.nvim',
+    lazy = true,
+    version = false, -- last release is way too old
+  },
   {
     'neovim/nvim-lspconfig',
     opts = {
       servers = {
         yamlls = {
+          -- lazy-load schemastore when needed
+          on_new_config = function(new_config)
+            new_config.settings.yaml.schemas = vim.tbl_deep_extend(
+              "force",
+              new_config.settings.yaml.schemas or {},
+              require("schemastore").yaml.schemas()
+            )
+          end,
           settings = {
+            redhat = { telemetry = { enabled = false } },
             yaml = {
-              schemaStore = {
+              keyOrdering = false,
+              format = {
                 enable = true,
-                url = 'https://www.schemastore.org/api/json/catalog.json',
+              },
+              validate = true,
+              schemaStore = {
+                -- Must disable built-in schemaStore support to use
+                -- schemas from SchemaStore.nvim plugin
+                enable = false,
+                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                url = '',
+              },
+            },
+          },
+          capabilities = {
+            textDocument = {
+              foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true,
               },
             },
           },
