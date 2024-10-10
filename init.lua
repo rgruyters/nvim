f0f205e6d8b3d4fb1e3d380ac20414329c2abb48
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-10-10 09:50:22 +0200

    chore(mason): remove mason-installer
    Not really using it as designed

--- init.lua
+++ init.lua
@@ -484,7 +484,6 @@ require('lazy').setup({
       -- Automatically install LSPs and related tools to stdpath for neovim
       { 'williamboman/mason.nvim', config = true },
       'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
@@ -672,20 +671,16 @@ require('lazy').setup({
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format lua code
-      })
-      require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
 
       require('mason-lspconfig').setup({
+        ensure_installed = ensure_installed,
+        automatic_installation = true,
         handlers = {
           function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
+            local server_opts = vim.tbl_deep_extend('force', {
+              capabilities = vim.deepcopy(capabilities),
+            }, servers[server_name] or {})
+            require('lspconfig')[server_name].setup(server_opts)
           end,
         },
       })
--- lua/custom/plugins/mason.lua
+++ lua/custom/plugins/mason.lua
@@ -3,19 +3,32 @@ return {
     'williamboman/mason.nvim',
     cmd = 'Mason',
     build = ':MasonUpdate',
+    opts_extend = { 'ensure_installed' },
     opts = {
-      ensure_installed = {},
+      ensure_installed = {
+        'stylua',
+        'shfmt',
+      },
     },
     config = function(_, opts)
       require('mason').setup(opts)
       local mr = require('mason-registry')
+
+      mr:on('package:install:success', function()
+        vim.defer_fn(function()
+          -- trigger FileType event to possibly load this newly installed LSP server
+          require('lazy.core.handler.event').trigger({
+            event = 'FileType',
+            buf = vim.api.nvim_get_current_buf(),
+          })
+        end, 100)
+      end)
 
       -- check if packages need to be installed
       mr.refresh(function()
         for _, pkg_name in ipairs(opts.ensure_installed) do
           local pkg = mr.get_package(pkg_name)
           if not pkg:is_installed() then
-            vim.notify(string.format('Installing %s', pkg_name), vim.log.levels.INFO, { title = 'mason' })
             pkg:install()
           else
             -- update installed package to latest version
