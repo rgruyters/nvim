0570b65df64f76bce86c97d43f5b43796b49781c
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-10-10 10:13:20 +0200

    refactor(nvim-lint): pre action for installing linting
    Do not pass ensure_installed to Mason, but rather by filetype

--- lua/custom/extras/lang/terraform.lua
+++ lua/custom/extras/lang/terraform.lua
@@ -9,18 +9,10 @@ return {
     end,
   },
   {
-    'williamboman/mason.nvim',
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { 'terraform-ls' })
-    end,
-  },
-  {
     'neovim/nvim-lspconfig',
     opts = {
       servers = {
-        terraformls = {
-        },
+        terraformls = {},
       },
     },
   },
