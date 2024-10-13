eb097f3132ad1353242d92beb338d08b8dd3b323
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-10-13 17:05:33 +0200

    chore(options): smartindent and update format
    Do not add comment when typing 'o'

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -10,6 +10,12 @@ vim.opt.tabstop = 4
 -- Number of spaces that a tab counts for while performing editing operations
 vim.opt.softtabstop = 4
 
+-- Do smart autoindenting when starting a new line
+vim.opt.smartindent = true
+
+-- Don't have `o` add a comment
+vim.opt.formatoptions:remove('o')
+
 -- Netrw settings
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
