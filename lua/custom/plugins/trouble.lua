19586b22d54f5b8d15a0dbb63173b5f573a01ead
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-10-16 16:23:45 +0200

    fix(trouble): correct key bindings

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -5,17 +5,25 @@ return {
   {
     'folke/trouble.nvim',
     cmd = { 'TroubleToggle', 'Trouble' },
-    opts = { use_diagnostic_signs = true },
-    keys = {
-      { '<leader>xx', '<cmd>TroubleToggle document_diagnostics<cr>', desc = 'Document Diagnostics (Trouble)' },
-      { '<leader>xX', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Workspace Diagnostics (Trouble)' },
-      { '<leader>xL', '<cmd>TroubleToggle loclist<cr>', desc = 'Location List (Trouble)' },
-      { '<leader>xQ', '<cmd>TroubleToggle quickfix<cr>', desc = 'Quickfix List (Trouble)' },
-      {
+    ---@module 'trouble'
+    ---@class trouble.Config
+    opts = {},
+    keys = function()
+      local trouble = require('trouble')
+      local keys = {
+        { '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Document Diagnostics (Trouble)' },
+        { '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Workspace Diagnostics (Trouble)' },
+        { '<leader>cs', '<cmd>Trouble symbols toggle<cr>', desc = 'Symbols (Trouble)' },
+        { '<leader>cS', '<cmd>Trouble lsp toggle<cr>', desc = 'LSP references/definitions/... (Trouble)' },
+        { '<leader>xL', '<cmd>Trouble loclist toggle<cr>', desc = 'Location List (Trouble)' },
+        { '<leader>xQ', '<cmd>Trouble qflist toggle<cr>', desc = 'Quickfix List (Trouble)' },
+      }
+
+      table.insert(keys, {
         '[q',
         function()
-          if require('trouble').is_open() then
-            require('trouble').previous({ skip_groups = true, jump = true })
+          if trouble.is_open() then
+            trouble.prev({ skip_groups = true, jump = true })
           else
             local ok, err = pcall(vim.cmd.cprev)
             if not ok then
@@ -24,8 +32,8 @@ return {
           end
         end,
         desc = 'Previous trouble/quickfix item',
-      },
-      {
+      })
+      table.insert(table, {
         ']q',
         function()
           if require('trouble').is_open() then
@@ -38,8 +46,10 @@ return {
           end
         end,
         desc = 'Next trouble/quickfix item',
-      },
-    },
+      })
+
+      return keys
+    end,
   },
 }
 
