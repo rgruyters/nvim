a565c30e1b5904a3c3aca0fcdc0982d284d37c30
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-10-16 16:23:02 +0200

    perf: update events for treesitter and vim-sleuth

--- lua/kickstart/plugins/vim-sleuth.lua
+++ lua/kickstart/plugins/vim-sleuth.lua
@@ -1,6 +1,7 @@
 return {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  event = { 'BufEnter' },
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
