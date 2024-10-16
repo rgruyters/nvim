14cbb54394ebce6c789e3f7bd71b64e9a4bedb5a
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-10-16 16:23:02 +0200

    perf: update events for treesitter and vim-sleuth

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    event = { 'BufReadPre', 'BufNewFile' },
     build = ':TSUpdate',
     opts = {
       ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
