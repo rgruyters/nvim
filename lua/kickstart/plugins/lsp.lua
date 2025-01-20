8c1de261533bba40af248f7d4526320e2d07f04b
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2025-01-20 11:30:58 +0100

    fix(lsp): use luals 3rd library for luv

--- lua/kickstart/plugins/lsp.lua
+++ lua/kickstart/plugins/lsp.lua
@@ -8,11 +8,10 @@ return {
     opts = {
       library = {
         -- Load luvit types when the `vim.uv` word is found
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
       },
     },
   },
-  { 'Bilal2453/luvit-meta', lazy = true },
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
