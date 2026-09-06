49c86af827bb787277bd34f2612471530d55ca96
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2026-09-06 16:04:12 +0200

    feat(plugin): add treesj

---
+++ plugin/treesj.lua
@@ -0,0 +1,4 @@
+-- Plugin for splitting/joining blocks of code like arrays, hashes, statements, objects, dictionaries, etc.
+vim.pack.add({ 'https://github.com/Wansmer/treesj' })
+
+require('treesj').setup()
